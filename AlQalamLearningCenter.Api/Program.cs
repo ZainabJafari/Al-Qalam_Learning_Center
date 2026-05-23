bec25ce7dfd9bcae33984e6da65c6ab0aec33f7c
Zainab Jafari <zainabjafari998@gmail.com>
2026-05-23 19:53:12 +0200

    add

---
+++ AlQalamLearningCenter.Api/Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/health", () =>
+{
+    return Results.Ok(new
+    {
+        status = "ok",
+        service = "AlQalamLearningCenter.Api",
+        timestamp = DateTimeOffset.UtcNow
+    });
+})
+.WithName("HealthCheck");
+
+app.Run();
