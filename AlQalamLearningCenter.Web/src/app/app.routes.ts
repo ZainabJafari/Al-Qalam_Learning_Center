f923d8be35752c13b77cb00054de27809358b63d
Zainab Jafari <zainabjafari998@gmail.com>
2026-06-22 11:30:49 +0200

    added

--- AlQalamLearningCenter.Web/src/app/app.routes.ts
+++ AlQalamLearningCenter.Web/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { AboutPageComponent } from './pages/about/about-page.component';
 import { ContactPageComponent } from './pages/contact/contact-page.component';
+import { CoursesPageComponent } from './pages/courses/courses-page.component';
 import { DonationCancelPageComponent } from './pages/donation-cancel/donation-cancel-page.component';
 import { DonationSuccessPageComponent } from './pages/donation-success/donation-success-page.component';
 import { DonatePageComponent } from './pages/donate/donate-page.component';
@@ -18,6 +19,10 @@ export const routes: Routes = [
   {
     path: 'about',
     component: AboutPageComponent
+  },
+  {
+    path: 'courses',
+    component: CoursesPageComponent
   },
   {
     path: 'contact',
